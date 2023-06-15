aba81d17b36dd3a86e8f9ac58a0192b4111f34ee
BrijeshNMeena <brijeshmeena1606@gmail.com>
2023-06-15 18:34:20 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+import java.io.*;
+
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        // rw.name = "changingPrivateMember";
+        // System.out.println(rw.name);
+        // 'name' has private access in 'com.driver.RWOnly'
+
+        rwOnly.setName("new_name");
+        String given_name = rwOnly.getName();
+        System.out.println(given_name);
+    }
+
 }
